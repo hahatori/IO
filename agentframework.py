17d1631fae57b4cbf69766c73315ea99ef66d0de
hahatori <56164812+hahatori@users.noreply.github.com>
2020-01-06 17:25:04 +0000

    Update agentframework.py

--- agentframework.py
+++ agentframework.py
@@ -1,15 +1,17 @@
+# Import the random module using the import statement.
 import random 
 
-# Build the class.
+# Build the Agent class.
 class Agent(): 
     
-    # Define methods.
-    def __init__(self, environment):
-        self.environment = environment
+    # # Define the initialization method.
+    def __init__(self, environment):  # The first argument of __init__ is always self, environment is formal parameter.
+        self.environment = environment # Pass the environment list to the Agent's constructor.
         self.store = 0
-        self.y = random.randint(0,99)
-        self.x = random.randint(0,99)
-        
+        self.y = random.randint(0,99) # Generate a random number between 0 and 99 and assign it to y.
+        self.x = random.randint(0,99) # Generate a random number between 0 and 99 and assign it to x.
+    
+    # Define the move method, use if...else statement and Torus to deal with boundary issues.
     def move(self):
         if random.random() < 0.5:
             self.x = (self.x + 1) % 100
@@ -20,7 +22,8 @@ class Agent():
             self.y = (self.y + 1) % 100
         else:
             self.y = (self.y - 1) % 100
-    
+            
+    # Define the eat method, use if statement to work out.
     def eat(self):
         if self.environment[self.y][self.x] > 10:
             self.environment[self.y][self.x] -= 10
@@ -29,6 +32,7 @@ class Agent():
     # This method returns a string repersenting that object when we use the print statement.   
     def __str__(self):
        return "y= %s, x= %s" % (self.y, self.x)
+
 
 # Creat objects to test print.           
 a = Agent("environment")
